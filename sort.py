13663dde2194383d27cf13cb57a7caaac3d817ca
maxmarkov <maxmarkov@gmail.com>
2020-09-06 02:13:20 +0200

    sort updated

--- sort.py
+++ sort.py
@@ -20,8 +20,8 @@ from __future__ import print_function
 import os
 import numpy as np
 import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
+#matplotlib.use('TkAgg')
+#import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from skimage import io
 
@@ -276,13 +276,13 @@ if __name__ == '__main__':
   total_time = 0.0
   total_frames = 0
   colours = np.random.rand(32, 3) #used only for display
-  if(display):
-    if not os.path.exists('mot_benchmark'):
-      print('\n\tERROR: mot_benchmark link not found!\n\n    Create a symbolic link to the MOT benchmark\n    (https://motchallenge.net/data/2D_MOT_2015/#download). E.g.:\n\n    $ ln -s /path/to/MOT2015_challenge/2DMOT2015 mot_benchmark\n\n')
-      exit()
-    plt.ion()
-    fig = plt.figure()
-    ax1 = fig.add_subplot(111, aspect='equal')
+  #if(display):
+  #  if not os.path.exists('mot_benchmark'):
+  #    print('\n\tERROR: mot_benchmark link not found!\n\n    Create a symbolic link to the MOT benchmark\n    (https://motchallenge.net/data/2D_MOT_2015/#download). E.g.:\n\n    $ ln -s /path/to/MOT2015_challenge/2DMOT2015 mot_benchmark\n\n')
+  #    exit()
+  #  plt.ion()
+  #  fig = plt.figure()
+  #  ax1 = fig.add_subplot(111, aspect='equal')
 
   if not os.path.exists('output'):
     os.makedirs('output')
@@ -302,11 +302,11 @@ if __name__ == '__main__':
         dets[:, 2:4] += dets[:, 0:2] #convert to [x1,y1,w,h] to [x1,y1,x2,y2]
         total_frames += 1
 
-        if(display):
-          fn = 'mot_benchmark/%s/%s/img1/%06d.jpg'%(phase, seq, frame)
-          im =io.imread(fn)
-          ax1.imshow(im)
-          plt.title(seq + ' Tracked Targets')
+        #if(display):
+        #  fn = 'mot_benchmark/%s/%s/img1/%06d.jpg'%(phase, seq, frame)
+        #  im =io.imread(fn)
+        #  ax1.imshow(im)
+        #  plt.title(seq + ' Tracked Targets')
 
         start_time = time.time()
         trackers = mot_tracker.update(dets)
@@ -319,10 +319,10 @@ if __name__ == '__main__':
             d = d.astype(np.int32)
             ax1.add_patch(patches.Rectangle((d[0],d[1]),d[2]-d[0],d[3]-d[1],fill=False,lw=3,ec=colours[d[4]%32,:]))
 
-        if(display):
-          fig.canvas.flush_events()
-          plt.draw()
-          ax1.cla()
+        #if(display):
+        #  fig.canvas.flush_events()
+        #  plt.draw()
+        #  ax1.cla()
 
   print("Total Tracking took: %.3f seconds for %d frames or %.1f FPS" % (total_time, total_frames, total_frames / total_time))
 
